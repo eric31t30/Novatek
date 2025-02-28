d597b2b30a24cffb441dfb1223e716eb11625647
Eric Mateo <eric31dev@gmail.com>
2025-02-28 08:57:36 -0500

    feat: complete stats counter design on home page✨

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { OverviewComponent } from '../overview/overview.component';
+import { StatsCounterComponent } from '../stats-counter/stats-counter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent,OverviewComponent],
+  imports: [HeroComponent, OverviewComponent, StatsCounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
