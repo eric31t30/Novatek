62a75fd9ed71e2eed0fb8d3789771619277a02c3
Eric Mateo <eric31dev@gmail.com>
2025-03-08 07:58:57 -0500

    feat: complete testimonials design on home page ✨

--- src/app/components/home/testimonials/testimonials.component.ts
+++ src/app/components/home/testimonials/testimonials.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { register } from 'swiper/element/bundle';
 
+
+register();
 @Component({
   selector: 'app-testimonials',
   imports: [CommonModule],
   templateUrl: './testimonials.component.html',
-  styleUrl: './testimonials.component.css'
+  styleUrl: './testimonials.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class TestimonialsComponent {
 
+export class TestimonialsComponent {
+  
 }
