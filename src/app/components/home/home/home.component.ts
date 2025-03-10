525f86cfc0c8108e0178a083bfa216e840628c62
Eric Mateo <eric31dev@gmail.com>
2025-03-10 09:35:12 -0500

    feat: add component home-contact ✨

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -3,10 +3,11 @@ import { HeroComponent } from '../hero/hero.component';
 import { OverviewComponent } from '../overview/overview.component';
 import { StatsCounterComponent } from '../stats-counter/stats-counter.component';
 import { TestimonialsComponent } from '../testimonials/testimonials.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, OverviewComponent, StatsCounterComponent, TestimonialsComponent],
+  imports: [HeroComponent, OverviewComponent, StatsCounterComponent, TestimonialsComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
