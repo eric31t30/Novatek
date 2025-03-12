e2af92424feb8dac16ffafed7a80e26324056366
Eric Mateo <eric31dev@gmail.com>
2025-03-12 08:28:48 -0500

    feat: add component footer ✨

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -4,10 +4,11 @@ import { OverviewComponent } from '../overview/overview.component';
 import { StatsCounterComponent } from '../stats-counter/stats-counter.component';
 import { TestimonialsComponent } from '../testimonials/testimonials.component';
 import { ContactComponent } from '../contact/contact.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, OverviewComponent, StatsCounterComponent, TestimonialsComponent, ContactComponent],
+  imports: [HeroComponent, OverviewComponent, StatsCounterComponent, TestimonialsComponent, ContactComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
