f955ed9227e142a3db874f5618f9b683dfa79116
Eric Mateo <eric31dev@gmail.com>
2025-03-15 08:28:15 -0500

    feat: add mission and vision component ✨

--- src/app/components/about-us/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us/about-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WhoWeAreComponent } from '../who-we-are/who-we-are.component';
 import { HistoryComponent } from '../history/history.component';
+import { MissionVisionComponent } from '../mission-vision/mission-vision.component';
 
 @Component({
   selector: 'app-about-us',
-  imports: [WhoWeAreComponent, HistoryComponent],
+  imports: [WhoWeAreComponent, HistoryComponent, MissionVisionComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
