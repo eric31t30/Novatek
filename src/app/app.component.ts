e85847121189f44d5d4b1953c9c61fcd537a2123
Eric Mateo <eric31dev@gmail.com>
2025-03-18 08:49:20 -0500

    feat: add services page and service-list component ✨

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home/home.component';
 import { AboutUsComponent } from './components/about-us/about-us/about-us.component';
+import { ServicesComponent } from './components/services/services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, AboutUsComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, AboutUsComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   
---
+++ src/app/components/services/services/services.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ServiceListComponent } from '../service-list/service-list.component';
+
+@Component({
+  selector: 'app-services',
+  imports: [ServiceListComponent],
+  templateUrl: './services.component.html',
+  styleUrl: './services.component.css'
+})
+export class ServicesComponent {
+
+}
