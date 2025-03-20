2f84cad77f88d867ed927d4a2d0431937b0f8e3b
Eric Mateo <eric31dev@gmail.com>
2025-03-20 09:54:39 -0500

    feat: add contact page and contact-form component ✨

---
+++ src/app/components/contact/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
+
+@Component({
+  selector: 'app-contact',
+  imports: [ContactFormComponent],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+}
