0955aa6eae89252af4f84ab5a0fb7a80a41a3333
Eric Mateo <eric31dev@gmail.com>
2025-03-20 08:40:06 -0500

    feat: add project-contact section services page ✨

--- src/app/components/services/services/services.component.ts
+++ src/app/components/services/services/services.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ServiceListComponent } from '../service-list/service-list.component';
 import { ClientsComponent } from '../clients/clients.component';
+import { ProjectContactComponent } from '../project-contact/project-contact.component';
 
 @Component({
   selector: 'app-services',
-  imports: [ServiceListComponent, ClientsComponent],
+  imports: [ServiceListComponent, ClientsComponent, ProjectContactComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
