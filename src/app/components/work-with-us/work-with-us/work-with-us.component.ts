629e7c2d2a1b5bfd127d3188ca828482834167db
Eric Mateo <eric31dev@gmail.com>
2025-03-27 10:00:56 -0500

    feat: add job-form section work-with-us page ✨

--- src/app/components/work-with-us/work-with-us/work-with-us.component.ts
+++ src/app/components/work-with-us/work-with-us/work-with-us.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HomeComponent } from '../../home/home/home.component';
 import { HeroComponent } from '../hero/hero.component';
 import { OpenJobsComponent } from '../open-jobs/open-jobs.component';
+import { JobFormComponent } from '../job-form/job-form.component';
 
 @Component({
   selector: 'app-work-with-us',
-  imports: [HeroComponent, OpenJobsComponent],
+  imports: [HeroComponent, OpenJobsComponent, JobFormComponent],
   templateUrl: './work-with-us.component.html',
   styleUrl: './work-with-us.component.css'
 })
