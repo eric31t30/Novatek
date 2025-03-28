0362e74fa2dd91d561832bf3f20ba373c0c81f91
Eric Mateo <eric31dev@gmail.com>
2025-03-28 09:55:10 -0500

    feat: add benefits section work-with-us page ✨

--- src/app/components/work-with-us/work-with-us/work-with-us.component.ts
+++ src/app/components/work-with-us/work-with-us/work-with-us.component.ts
@@ -3,10 +3,11 @@ import { HomeComponent } from '../../home/home/home.component';
 import { HeroComponent } from '../hero/hero.component';
 import { OpenJobsComponent } from '../open-jobs/open-jobs.component';
 import { JobFormComponent } from '../job-form/job-form.component';
+import { BenefitsComponent } from "../benefits/benefits.component";
 
 @Component({
   selector: 'app-work-with-us',
-  imports: [HeroComponent, OpenJobsComponent, JobFormComponent],
+  imports: [HeroComponent, OpenJobsComponent, JobFormComponent, BenefitsComponent],
   templateUrl: './work-with-us.component.html',
   styleUrl: './work-with-us.component.css'
 })
