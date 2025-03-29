0b5392c0f3c351de18ad4fd56dcaebb35d54d455
Eric Mateo <eric31dev@gmail.com>
2025-03-29 09:40:09 -0500

    feat: add culture section work-with-us page ✨

--- src/app/components/work-with-us/work-with-us/work-with-us.component.ts
+++ src/app/components/work-with-us/work-with-us/work-with-us.component.ts
@@ -4,10 +4,11 @@ import { HeroComponent } from '../hero/hero.component';
 import { OpenJobsComponent } from '../open-jobs/open-jobs.component';
 import { JobFormComponent } from '../job-form/job-form.component';
 import { BenefitsComponent } from "../benefits/benefits.component";
+import { CultureComponent } from '../culture/culture.component';
 
 @Component({
   selector: 'app-work-with-us',
-  imports: [HeroComponent, OpenJobsComponent, JobFormComponent, BenefitsComponent],
+  imports: [HeroComponent, OpenJobsComponent, JobFormComponent, BenefitsComponent, CultureComponent],
   templateUrl: './work-with-us.component.html',
   styleUrl: './work-with-us.component.css'
 })
