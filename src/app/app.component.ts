3b9d64d86e039430d391ddab829e6073cabc85ef
Eric Mateo <eric31dev@gmail.com>
2025-04-27 14:09:30 -0500

    feat: Add scroll-to-top behavior on route navigation and routing in footer ✨

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home/home.component';
-import { AboutUsComponent } from './components/about-us/about-us/about-us.component';
-import { ServicesComponent } from './components/services/services/services.component';
-import { ContactComponent } from './components/contact/contact/contact.component';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
+
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
-import { WorkWithUsComponent } from './components/work-with-us/work-with-us/work-with-us.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, AboutUsComponent, ServicesComponent, ContactComponent, WorkWithUsComponent,HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   
 })
 export class AppComponent {
   title = 'novatek';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo({ top: 0, behavior: 'instant' });
+      });
+  }
 }
