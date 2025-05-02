aa27fa33c9dc6b456b3c439f8b05b09b1ced2da0
Eric Mateo <eric31dev@gmail.com>
2025-05-02 07:14:38 -0500

    feat: add jobs-data and iterate open-jobs offers based on it ✨

--- src/app/components/work-with-us/open-jobs/open-jobs.component.ts
+++ src/app/components/work-with-us/open-jobs/open-jobs.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { JOBS } from '../../../data/jobs.data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-open-jobs',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './open-jobs.component.html',
   styleUrl: './open-jobs.component.css'
 })
 export class OpenJobsComponent {
+
+  jobs = JOBS;
 
 }
