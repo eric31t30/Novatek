df6e8d4aa75276bce47bd06f1716ecfdf3d502cc
Eric Mateo <eric31dev@gmail.com>
2025-05-05 09:57:37 -0500

    feat: add counters logic in stats-counter component ✨

--- src/app/components/home/stats-counter/stats-counter.component.ts
+++ src/app/components/home/stats-counter/stats-counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-stats-counter',
@@ -6,6 +6,36 @@ import { Component } from '@angular/core';
   templateUrl: './stats-counter.component.html',
   styleUrl: './stats-counter.component.css'
 })
-export class StatsCounterComponent {
+export class StatsCounterComponent implements AfterViewInit {
 
+  @ViewChild('countersSection') countersSection!: ElementRef;
+  
+  clients = 0;
+  years = 0;
+  projects = 0;
+
+  private observer!: IntersectionObserver;
+
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        this.count('clients', 70);
+        this.count('years', 12);
+        this.count('projects', 50);
+        this.observer.disconnect();
+      }
+    }, { threshold: 0.5 });
+
+    this.observer.observe(this.countersSection.nativeElement);
+  }
+
+  count(field: 'projects' | 'years' | 'clients', final: number) {
+    let value = 0;
+    const interval = setInterval(() => {
+      this[field]++;
+      value++;
+      if (value >= final) clearInterval(interval);
+    }, 1500 / final);
+  }
+  
 }
