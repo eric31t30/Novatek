c3a037641cc9f759776b7f230b4f3146ec5a1774
Eric Mateo <eric31dev@gmail.com>
2025-05-08 09:54:07 -0500

    feat: add routing to terms-and-conditions page in footer ✨

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,11 @@ import { filter } from 'rxjs';
 
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
-import { TermsAndConditionsComponent } from './components/terms-and-conditions/terms-and-conditions.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, TermsAndConditionsComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   
