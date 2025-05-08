1172812f6bd51ac0d1ff215198f7760104f1f830
Eric Mateo <eric31dev@gmail.com>
2025-05-08 09:43:07 -0500

    feat: add terms-and-conditions component ✨

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { filter } from 'rxjs';
 
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
+import { TermsAndConditionsComponent } from './components/terms-and-conditions/terms-and-conditions.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, TermsAndConditionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   
