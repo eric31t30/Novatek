38de6bb94042ae0c2529832eeef4047a76f40c8b
Eric Mateo <eric31dev@gmail.com>
2025-05-23 08:46:36 -0500

    feat: add our-values section ✨

---
+++ src/app/components/about-us/our-values/our-values.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-values',
+  imports: [],
+  templateUrl: './our-values.component.html',
+  styleUrl: './our-values.component.css'
+})
+export class OurValuesComponent {
+
+}
