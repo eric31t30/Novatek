1145166d90fbcd3691838246106a54d76270629c
Eric Mateo <eric31dev@gmail.com>
2025-06-03 09:15:45 -0500

    refactor: rework service-list section for improved layout ♻️

---
+++ src/app/data/services.data.ts
@@ -0,0 +1,33 @@
+export const SERVICES = [
+    {
+      title: 'Desarrollo Web',
+      description: 'Diseñamos y desarrollamos sitios web responsivos, modernos y optimizados para ofrecer una gran experiencia en cualquier dispositivo.',
+      icon: '/assets/services-page/service-list/web-development.svg' 
+    },
+    {
+      title: 'Desarrollo de Software',
+      description: 'Creamos aplicaciones web y móviles a medida, seguras, escalables y con alto rendimiento.',
+      icon:  '/assets/services-page/service-list/software.svg'
+    },
+    {
+      title: 'Mantenimiento y Soporte',
+      description: 'Ofrecemos soporte técnico y mantenimiento continuo para asegurar que tus sistemas estén siempre actualizados y funcionando correctamente.',
+      icon: '/assets/services-page/service-list/support.svg'
+    },
+    {
+      title: 'Diseño UI/UX',
+      description: 'Diseñamos interfaces intuitivas, visualmente atractivas y centradas en el usuario para mejorar la experiencia digital.',
+      icon: '/assets/services-page/service-list/design.svg'
+    },
+    {
+      title: 'Inteligencia Artificial',
+      description: 'Aplicamos IA para automatizar procesos, analizar datos y crear soluciones inteligentes como chatbots y sistemas de recomendación.',
+      icon: '/assets/services-page/service-list/ai.svg'
+    },
+    {
+      title: 'Análisis y Gestión de Datos',
+      description: 'Optimizamos el uso de tus datos con soluciones para su almacenamiento, análisis y visualización, facilitando decisiones estratégicas.',
+      icon: '/assets/services-page/service-list/analysis.svg'
+    },
+  ];
+  
