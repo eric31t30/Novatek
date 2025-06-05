dfefc3c7a06370ffe01d1f51484ea47ab5dea86d
Eric Mateo <eric31dev@gmail.com>
2025-06-05 13:26:41 -0500

    feat: complete scroll-to-top (button) ✨

--- src/app/components/scroll-to-top/scroll-to-top.component.ts
+++ src/app/components/scroll-to-top/scroll-to-top.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-scroll-to-top',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './scroll-to-top.component.html',
   styleUrl: './scroll-to-top.component.css'
 })
 export class ScrollToTopComponent {
 
+  isVisible = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isVisible = scrollY > 1100;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
