f16131954c8e32db1ad3b13a9998f33a152cec5f
Eric Mateo <eric31dev@gmail.com>
2025-06-07 13:49:38 -0500

    feat: add basic loader ✨

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 import { filter } from 'rxjs';
 
 
@@ -8,11 +9,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { CookiesComponent } from './components/cookies/cookies.component';
 import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CookiesComponent, ScrollToTopComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CookiesComponent, ScrollToTopComponent, LoaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   
@@ -20,11 +22,29 @@ import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.c
 export class AppComponent {
   title = 'novatek';
 
+  isLoading = false;
+  private loaderTimeout: any;
+
   constructor(private router: Router) {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe(() => {
       window.scrollTo({ top: 0, behavior: 'instant' });
+    });
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loaderTimeout = setTimeout(() => {
+          this.isLoading = true;
+        }, 200);
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        clearTimeout(this.loaderTimeout);
+        this.isLoading = false;
+      }
     });
   }
 }
